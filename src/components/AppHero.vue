28833768dbaa78808d48e581733d6b2ce1a81b13
Pietro Giordano <giopi87@gmail.com>
2023-02-01 18:44:27 +0100

    fine esercizio base

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+      data() {
+            return {
+
+            }
+      }
+}
+</script>
+
+<template>
+      <div class="jumbotron"></div>
+</template>
+
+<style scoped>
+.jumbotron {
+      width: 100%;
+      height: 100vh;
+      background-image: url('tiemporeal.periodismoudec.cl/wp-content/uploads/2022/09/The-Boys-temporada-3-esta-loca-teoria-sobre-los-planes.jpeg');
+}
+</style>
